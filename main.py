fd7683c88a28e953ae45a2042969abf3368fad66
Kangyoo <e.kangyoo@gmail.com>
2021-07-09 15:02:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ f'''이름 : {config['name']}
     else:
         print('존재하지 않는 명령어 입니다.')
 
-print('\033[31m'+f'''@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+print(f'''@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 Made by Kangyoo Lee ( https://github.com/kang309 )
 이 프로그램은 hcskr 자가진단 라이브러리를 이용해 만들었습니다. ( https://github.com/331leo/hcskr_python )
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-'''+'\033[0m'+
+'''+
 f'''
 이름 : {config['name']}
 생년월일 : {config['birth']}
@@ -56,26 +56,27 @@ if answer == 'Y' or answer == 'y' or answer == 'yes':
 프로그램을 실행하겠습니다!
 ''')
     
-    print('\033[96m'+
+    print(
 '''~~~~~~~~~~~< 사용법 >~~~~~~~~~~~
 time 또는 t 입력 -> 현재시간 출력
 status 또는 s 입력 -> 설정내용 출력
 stop 입력 -> 프로그램 종료
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''+'\033[0m')
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~''')
     while True:
 
         while True:
 
             localtime = time.localtime()
             result = time.strftime("%I:%M:%S %p", localtime)
-            print('>>>',end=' ')
-            option = str(input())
-            command_check(option)
 
             if result == config['time']:
                 asyncio.get_event_loop().run_until_complete(check())
                 print('자가진단 완료. ( 현재 시각 : {0} )'.format(result))
                 break;
+            
+            print('>>>',end=' ')
+            option = str(input())
+            command_check(option)
 
 else:
     print('Abort.')
